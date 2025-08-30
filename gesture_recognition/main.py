fa4d480a397675201cdebebf8bea02461670b2c0
AimanHaidar <atahalabsi@gmail.com>
2025-08-31 01:56:04 +0300

    combine mqtt with gesture recognition in main

--- gesture_recognition/main.py
+++ gesture_recognition/main.py
@@ -1,6 +1,51 @@
-def main():
-    print("Hello from motor-control-via-hand-gesture-with-rtos!")
+import pickle
+import numpy as np
+from src.models.hand_gesture_detector import HandGestureDetector
+import threading
+import pandas as pd
+import time
 
+import paho.mqtt.client as mqtt
+
+BROKER = "192.168.0.81"  # same as mqtt_server in ESP
+PORT = 1883
+TOPIC = "esp32/command/velocity-mode"
+
+def on_message(client, userdata, msg):
+    print(f"Received: {msg.payload.decode()} from topic {msg.topic}")
+
+
+
+
+def thumb_gesture(detector,publish):
+    # Load the model
+   with open("models/rf_model.pkl", "rb") as f:
+        model = pickle.load(f)
+        header = ["p1_x","p1_y","p2_x","p2_y","p3_x","p3_y","p4_x","p4_y"]
+        last_time = time.time()
+        while not detector.closed:
+            if not detector.running or (time.time() - last_time) < 0.1:
+                time.sleep(0.05)
+                continue
+            
+            sample = [coord for p in detector.indecies_relative_to_wrist[1:5] for coord in p]
+
+            # Wrap sample in DataFrame with same columns
+            df_sample = pd.DataFrame([sample], columns=header)
+
+            prediction = model.predict(df_sample)
+            publish(TOPIC,f"Prediction: {prediction[0]}")
 
 if __name__ == "__main__":
-    main()
+    # Create client and connect
+    client = mqtt.Client(protocol=mqtt.MQTTv311)
+    client.connect(BROKER, PORT, 60)
+
+    detector = HandGestureDetector("http://192.168.0.75:8080/video")
+    threading.Thread(target=thumb_gesture,
+                    args=(detector,client.publish,),
+                    daemon=True).start()
+    
+    detector.run()
+
+    client.disconnect()
