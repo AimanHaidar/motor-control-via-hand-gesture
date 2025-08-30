aca985bb11b56cb80c94d91ba02ba81ab30cde12
AimanHaidar <atahalabsi@gmail.com>
2025-08-30 17:10:02 +0300

    frame all things in one class

--- gesture_recognition/src/hand_gesture_detector.py
+++ gesture_recognition/src/hand_gesture_detector.py
@@ -18,97 +18,100 @@ mp_hands = mp.solutions.hands
 # Provides helper functions to draw landmarks and connections on the image
 mp_drawing = mp.solutions.drawing_utils
 
-def get_finger_status(hand_landmarks):
-	# each point on hand have index for example:
-	# the wrist index is 0 the thumb finger [1,2,3,4] so it's tip is 4
-	# and the pip is what point we want to determain if the 
-	# finger is up or down
-	fingers = []
-	tips = [4, 8, 12, 16, 20] #the tips for each finger 
-	pip = [3, 6, 10, 14, 18]
-	front_hand_face = True
-	# to Check if the hand is flipped or not
-	if hand_landmarks.landmark[WRIST_INDEX].y > hand_landmarks.landmark[pip[MIDDLE]].y:
-		print(hand_landmarks.landmark[WRIST_INDEX].y," ",hand_landmarks.landmark[pip[MIDDLE]].y)
-		print("hand not flipped")
-		# to Check if it is the front or the back hand face
-		if hand_landmarks.landmark[pip[THUMB]].x > hand_landmarks.landmark[tips[PINKY]].x :
-			print("front_face")
-		# Thumb
-			fingers.append(1 if hand_landmarks.landmark[tips[THUMB]].x > hand_landmarks.landmark[pip[THUMB]].x else 0)
+class HandGestureDetector:
+	def __init__(self,camera_address=0,fps = 10 , max_num_hands=1, min_detection_confidence=0.3, min_tracking_confidence=0.6):
+		self.hands = mp_hands.Hands(
+			static_image_mode=False,
+			max_num_hands=max_num_hands,
+			min_detection_confidence=min_detection_confidence,
+			min_tracking_confidence=min_tracking_confidence
+		)
+
+		self.cap = cv2.VideoCapture(camera_address)
+		self.fps = fps
+
+
+	def get_finger_status(self,hand_landmarks):
+		# each point on hand have index for example:
+		# the wrist index is 0 the thumb finger [1,2,3,4] so it's tip is 4
+		# and the pip is what point we want to determain if the 
+		# finger is up or down
+		fingers = []
+		tips = [4, 8, 12, 16, 20] #the tips for each finger 
+		pip = [3, 6, 10, 14, 18]
+		front_hand_face = True
+		# to Check if the hand is flipped or not
+		if hand_landmarks.landmark[WRIST_INDEX].y > hand_landmarks.landmark[pip[MIDDLE]].y:
+			print(hand_landmarks.landmark[WRIST_INDEX].y," ",hand_landmarks.landmark[pip[MIDDLE]].y)
+			print("hand not flipped")
+			# to Check if it is the front or the back hand face
+			if hand_landmarks.landmark[pip[THUMB]].x > hand_landmarks.landmark[tips[PINKY]].x :
+				print("front_face")
+			# Thumb
+				fingers.append(1 if hand_landmarks.landmark[tips[THUMB]].x > hand_landmarks.landmark[pip[THUMB]].x else 0)
+			else:
+				print("back_face")
+				fingers.append(1 if hand_landmarks.landmark[tips[THUMB]].x < hand_landmarks.landmark[pip[THUMB]].x else 0)
+
+			# Other fingers
+			for i in range(1, 5):
+				fingers.append(1 if hand_landmarks.landmark[tips[i]].y < hand_landmarks.landmark[pip[i]].y else 0)
 		else:
-			print("back_face")
-			fingers.append(1 if hand_landmarks.landmark[tips[THUMB]].x < hand_landmarks.landmark[pip[THUMB]].x else 0)
+			print(hand_landmarks.landmark[WRIST_INDEX].y," ",hand_landmarks.landmark[pip[MIDDLE]].y)
+			print("hand flipped")
+			if hand_landmarks.landmark[pip[THUMB]].x < hand_landmarks.landmark[tips[PINKY]].x :
+				print("front_face")
+			# Thumb
+				fingers.append(1 if hand_landmarks.landmark[tips[THUMB]].x < hand_landmarks.landmark[pip[THUMB]].x else 0)
+			else:
+				print("back_face")
+				fingers.append(1 if hand_landmarks.landmark[tips[THUMB]].x > hand_landmarks.landmark[pip[THUMB]].x else 0)
 
-		# Other fingers
-		for i in range(1, 5):
-			fingers.append(1 if hand_landmarks.landmark[tips[i]].y < hand_landmarks.landmark[pip[i]].y else 0)
-	else:
-		print(hand_landmarks.landmark[WRIST_INDEX].y," ",hand_landmarks.landmark[pip[MIDDLE]].y)
-		print("hand flipped")
-		if hand_landmarks.landmark[pip[THUMB]].x < hand_landmarks.landmark[tips[PINKY]].x :
-			print("front_face")
-		# Thumb
-			fingers.append(1 if hand_landmarks.landmark[tips[THUMB]].x < hand_landmarks.landmark[pip[THUMB]].x else 0)
-		else:
-			print("back_face")
-			fingers.append(1 if hand_landmarks.landmark[tips[THUMB]].x > hand_landmarks.landmark[pip[THUMB]].x else 0)
-
-		# Other fingers
-		for i in range(1, 5):
-			fingers.append(1 if hand_landmarks.landmark[tips[i]].y > hand_landmarks.landmark[pip[i]].y else 0)
-	return fingers
-
-
-# ✅ Connect to phone stream
-#cap = cv2.VideoCapture("http://192.168.0.75:8080/video")
-cap = cv2.VideoCapture(0)
-
-# Skip frames to lower FPS
-TARGET_FPS = 10   # process ~10 frames per second
-last_time = 0
-
-# creates a Mediapipe Hands object (a model that detects and tracks hands)
-with mp_hands.Hands(
-	static_image_mode=False,
-	max_num_hands=1,
-	min_detection_confidence=0.3, #Minimum confidence for the initial hand detection (before tracking starts).
-	min_tracking_confidence=0.6 # Confidence threshold for tracking hand landmarks across frames.
-) as hands:
+			# Other fingers
+			for i in range(1, 5):
+				fingers.append(1 if hand_landmarks.landmark[tips[i]].y > hand_landmarks.landmark[pip[i]].y else 0)
+		return fingers
 	
-	while True:
-		ret, frame = cap.read()
-		if not ret:
-			break
+	def run(self):
+		last_time = 0
+		while True:
+			ret, frame = self.cap.read()
+			if not ret:
+				break
 
-		# Limit FPS
-		current_time = time.time()
-		if (current_time - last_time) < 1.0 / TARGET_FPS:
-			continue
-		last_time = current_time
+			# Limit FPS
+			current_time = time.time()
+			if (current_time - last_time) < 1.0 / self.fps:
+				continue
+			last_time = current_time
 
-		# ✅ Rotate if needed
-		#frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+			# ✅ Rotate if needed
+			#frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
 
-		# Flip for mirror effect
-		#frame = cv2.rotate(frame, 2)
+			# Flip for mirror effect
+			#frame = cv2.rotate(frame, 2)
 
-		rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-		results = hands.process(rgb)
+			rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+			results = self.hands.process(rgb)
 
-		if results.multi_hand_landmarks:
-			for hand_landmarks in results.multi_hand_landmarks:
-				mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+			if results.multi_hand_landmarks:
+				for hand_landmarks in results.multi_hand_landmarks:
+					mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
-				fingers = get_finger_status(hand_landmarks)
-				gesture = ''.join(str(f) for f in fingers)
-				cv2.putText(frame, f"{gesture}", (10, 50),
-							cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-				
-		cv2.imshow("Finger Gesture Detection", frame)
+					fingers = self.get_finger_status(hand_landmarks)
+					self.gesture = ''.join(str(f) for f in fingers)
+					cv2.putText(frame, f"{self.gesture}", (10, 50),
+								cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+					
+			cv2.imshow("Finger Gesture Detection", frame)
 
-		if cv2.waitKey(1) & 0xFF == ord('q'):  # ESC to exit
-			break
+			if cv2.waitKey(1) & 0xFF == ord('q'):  # ESC to exit
+				break
 
-cap.release()
-cv2.destroyAllWindows()
+		self.cap.release()
+		cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+	detector = HandGestureDetector(camera_address=0, fps=10)
+	detector.run()
